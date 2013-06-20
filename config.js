ef64cffe64722ee5cd06e0e9126e2cd9eaf0efa3
Sameer Akhtar <sameer.akhtar@one97.net>
2013-06-20 22:31:30 +0530

    countryapp

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+  countryserverurl  : 'http://localhost:4000/?country=',
+  countryserverport  : 4000
+};
+
+module.exports = config;
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var countries = require('./countries');
+
+
+module.exports = function(app) {
+  app.get('/', countries.getcountires);
+  app.get('/country/:name', countries.getcountry);
+
+};
