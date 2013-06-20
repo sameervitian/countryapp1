b6ea04516828f0292f52e8e0fb42aaf036681598
Sameer Akhtar <sameer.akhtar@one97.net>
2013-06-20 23:02:39 +0530

    description added

---
+++ README.md
@@ -0,0 +1,6 @@
+CountryApp
+==================
+
+Node.js app meant to demonstrate functionality of node.js + express.js + jade + request module
+Could be used for tutorial purpose.
+
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,4 @@ var countries = require('./countries');
 module.exports = function(app) {
   app.get('/', countries.getcountires);
   app.get('/country/:name', countries.getcountry);
-
 };
